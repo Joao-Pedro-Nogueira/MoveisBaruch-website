1d9a4420eb868c40db22bc0366e78d353c0a41f5
João Pedro Nogueira <jpedronogueira.dev@gmail.com>
2022-05-16 19:33:57 -0400

    add: main.js mod: home.html (navigation working)

---
+++ main.js
@@ -0,0 +1,10 @@
+// const nav = document.querySelector('#nav')
+// Isso não é necessário, pois o JS já considera o "ID" como objeto
+
+function onScroll() {
+  if (scrollY == 0) {
+    return nav.classList.remove('scroll')
+  } else {
+    return nav.classList.add('scroll')
+  }
+}
